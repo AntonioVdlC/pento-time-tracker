8ef08f55de0c522c60ecbb1f85ed56e42493d293
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-11-25 15:59:44 +0800

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+// Serve static files only in production
+if (process.env.NODE_ENV === 'production') {
+  const clientPath = 'client/build';
+
+  app.use(express.static(clientPath));
+
+  app.get('*', (req, res) => {
+    res.sendFile('/index.html', { root: clientPath });
+  });
+}
+
+// Start the server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Listening on port ${port} ...`);
+});

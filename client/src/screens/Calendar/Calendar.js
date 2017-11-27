73742a02150520977968cc8bf906eac697214507
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-11-27 14:38:56 +0800

    Display sessions in calendar (as a list for now)

--- client/src/screens/Calendar/Calendar.js
+++ client/src/screens/Calendar/Calendar.js
@@ -4,12 +4,46 @@ import { Link } from 'react-router-dom';
 import './Calendar.css';
 
 class Calendar extends Component {
-  state = {};
+  state = {
+    loading: true
+  };
+
+  componentDidMount = async () => {
+    const { store } = this.props;
+
+    try {
+      let sessions = await store.get('sessions');
+      this.setState(prev => ({ ...prev, sessions }));
+    } catch (err) {
+      this.setState(prev => ({ ...prev, error: true }));
+    } finally {
+      this.setState(prev => ({ ...prev, loading: false }));
+    }
+  };
+
   render = () => {
-    return (
+    const { loading, error, sessions } = this.state;
+    return loading ? (
+      <p>Loading...</p>
+    ) : (
       <div className="calendar">
-        Calendar<div className="time-tracker-footer">
-          <Link to="/">Back</Link>
+        {error ? <p>Oops, an error occured ... Please try again!</p> : null}
+        Calendar
+        {sessions && sessions.length ? (
+          <ul>
+            {sessions.map((session, index) => (
+              <li key={`session-${index}`}>
+                <p>{session.name}</p>
+                <p>{session.start}</p>
+                <p>{session.stop}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No sessions registered yet, please track a session first!</p>
+        )}
+        <div className="time-tracker-footer">
+          <Link to="/">Track a session</Link>
         </div>
       </div>
     );
